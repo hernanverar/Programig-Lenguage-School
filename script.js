9e5ba8e04321622de99c2840f26789f9fd990462
hernan verar <hernanverar@gmail.com>
2023-03-24 12:39:32 -0700

    WIP else if stament not working properly doing some research

--- script.js
+++ script.js
@@ -3,19 +3,21 @@ window.onload = function () {
   const form = document.querySelector("form")
   form.onsubmit = function(e) {
     e.preventDefault();
-  const Ruby = document.getElementById("Ruby").value;
-  // const Javascript = document.getElementById("Javascript").value;
+  const Lenguage = document.getElementById("Lenguage").value;
+  
 
-    let car = document.getElementById("car");
-    car.setAttribute("class", "hidden");
-    // let bus = document.getElementById("bus");
-    // bus.setAttribute("class", "hidden");
+    let bike = document.getElementById("bike");
+    bike.setAttribute("class", "hidden");
 
-    if (Ruby === "car") {
-      car.removeAttribute("class", "hidden");
-    // }else if (Javascript === bus) {
-    //   bus.removeAttribute("class", "hidden");
+    let bus = document.getElementById("bus");
+    bus.setAttribute("class", "hidden");
+
+    if (Lenguage === "bike") {
+      bike.removeAttribute("class", "hidden");
+
+    } else if (bus) {
+      bus.removeAttribute("class", "hidden");
     }
-  }
+  };
 
-}
+};
