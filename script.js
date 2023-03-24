da21e52c20b6219e642b2ef3af12a511eb2b508e
hernan verar <hernanverar@gmail.com>
2023-03-24 11:34:15 -0700

    WIP added funtionaly to our Javascript for our first survey question working on how to add next survey question

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+
 window.onload = function () {
   const form = document.querySelector("form")
   form.onsubmit = function(e) {
     e.preventDefault();
-    
-    let survey = document.getElementById("Ruby");
-    survey.setAttribute("class", "hidden");
+  const Ruby = document.getElementById("Ruby").value;
+  // const Javascript = document.getElementById("Javascript").value;
 
+    let car = document.getElementById("car");
+    car.setAttribute("class", "hidden");
+    // let bus = document.getElementById("bus");
+    // bus.setAttribute("class", "hidden");
+
+    if (Ruby === "car") {
+      car.removeAttribute("class", "hidden");
+    // }else if (Javascript === bus) {
+    //   bus.removeAttribute("class", "hidden");
+    }
   }
 
 }
