9439c1a50c8a102a76e792752d753f5ac1964990
hernan verar <hernanverar@gmail.com>
2023-03-24 13:58:24 -0700

    WIP added functionality to our survey div tag was open and was causing error in the page

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 
-window.onload = function () {
-  const form = document.querySelector("form")
-  form.onsubmit = function(e) {
+window.onload = () =>{
+  document.querySelector("form").onsubmit = (e) => {
     e.preventDefault();
-  const Lenguage = document.getElementById("Lenguage").value;
-  
 
-    let bike = document.getElementById("bike");
-    bike.setAttribute("class", "hidden");
+  const Lenguage = document.getElementById("Lenguage").value;
+  // const Lenguage1= document.getElementById("Lenguage1").value;
+
+    let car = document.getElementById("car");
+    car.setAttribute("class", "hidden");
 
     let bus = document.getElementById("bus");
     bus.setAttribute("class", "hidden");
+    let bike = document.getElementById("bike");
+    bike.setAttribute("class", "hidden");
+    let train = document.getElementById("train");
+    train.setAttribute("class", "hidden");
+    
 
-    if (Lenguage === "bike") {
-      bike.removeAttribute("class", "hidden");
+    if (Lenguage === "car") {
+        car.removeAttribute("class", "hidden");
+    } else if (Lenguage === "bus") {
+        bus.removeAttribute("class", "hidden");
+    } else if (Lenguage === "bike") {
+        bike.removeAttribute("class", "hidden");
+    } else if (Lenguage === "train") {
+        train.removeAttribute("class", "hidden");
+      }
 
-    } else if (bus) {
-      bus.removeAttribute("class", "hidden");
-    }
   };
 
 };
